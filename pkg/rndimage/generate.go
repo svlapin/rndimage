e074f791fff0d9f25f614aa385257dd0cbfac18d
Sergey Lapin <sv0lapin@gmail.com>
2019-07-29 23:28:25 +0200

    feat: add concurrency by a number of rows
    This makes generation ~2x faster.
    Described in the blog post
    https://svlapin.github.io/engineering/2019/07/29/optimizing-go-program-tracer-pprof.html

--- pkg/rndimage/generate.go
+++ pkg/rndimage/generate.go
@@ -4,26 +4,32 @@ import (
 	"image"
 	"image/color"
 	"math/rand"
+	"sync"
 )
-
-func randomUint8() uint8 {
-	return uint8(rand.Intn(256))
-}
 
 // Generate - generates `image.RGBA` of given `width` and `height` filling it with random pixel colors
 func Generate(width, height int) *image.RGBA {
 	rgba := image.NewRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{width, height}})
 
+	var wg sync.WaitGroup
+	wg.Add(width)
+
 	for i := 0; i < width; i++ {
-		for k := 0; k < height; k++ {
-			rgba.Set(i, k, color.RGBA{
-				R: randomUint8(),
-				G: randomUint8(),
-				B: randomUint8(),
-				A: randomUint8(),
-			})
-		}
+		go func(i int) {
+			rand := rand.New(rand.NewSource(rand.Int63()))
+			for k := 0; k < height; k++ {
+				rgba.Set(i, k, color.RGBA{
+					R: uint8(rand.Intn(256)),
+					G: uint8(rand.Intn(256)),
+					B: uint8(rand.Intn(256)),
+					A: uint8(rand.Intn(256)),
+				})
+			}
+			wg.Done()
+		}(i)
 	}
+
+	wg.Wait()
 
 	return rgba
 }
